ac5eed53b1a31e2873d2aca8143a36b47d34b053
mizanmahi <mizanmahi24@gmail.com>
2021-11-08 14:29:25 +0600

    add: user info saving route

--- server.js
+++ server.js
@@ -23,18 +23,56 @@ const main = async () => {
 
       const database = client.db('doctors-portal');
       const appointBookingCollection = database.collection('appointBookings');
-
+      const userCollection = database.collection('users');
 
       //@ APIs
 
+      app.get('/bookings', async (req, res) => {
+         const { email, date } = req.query;
+
+         const cursor = appointBookingCollection.find({
+            patientEmail: email,
+            date,
+         });
+
+         const bookings = await cursor.toArray();
+
+         res.json(bookings);
+      });
+
       app.post('/bookings', async (req, res) => {
          const appointBooking = req.body;
-         const { insertedId } = await appointBookingCollection.insertOne(appointBooking);
+         const { insertedId } = await appointBookingCollection.insertOne(
+            appointBooking
+         );
          appointBooking._id = insertedId;
          res.json(appointBooking);
       });
 
-   
+      app.post('/users', async (req, res) => {
+         const user = req.body;
+         const { insertedId } = await userCollection.insertOne(user);
+         user._id = insertedId;
+         res.json(user);
+      });
+
+      //@ this will be basically an upsert  operation for google login
+      app.put('/users', async (req, res) => {
+         const user = req.body;
+         const result = await userCollection.updateOne(
+            {
+               email: user.email,
+            },
+            {
+               $set: user,
+            },
+            {
+               upsert: true,
+            }
+         );
+         user._id = result.upsertedId;
+         res.json(user);
+      });
    } catch (err) {
       console.error({ dbError: err });
    } finally {
